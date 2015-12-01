abce52209ee7661bf742af37e5fbd9457dbeb3e6
Alpha2546 <adriaanvanloon@hotmail.com>
2015-12-01 23:00:24 +0100

    filling up the repo

---
+++ ui/mods/violetania/maplist.js
@@ -0,0 +1,5 @@
+﻿cShareSystems.load_pas("Violet", [
+	"coui://ui/mods/alpha2546/systems/crevage.pas",	
+	"coui://ui/mods/alpha2546/systems/andromeda.pas",	
+	"coui://ui/mods/alpha2546/systems/harbour.pas"
+]);
